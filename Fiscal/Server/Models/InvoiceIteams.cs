210dd7af20b0effc1043f92ce0c6d60582c00cda
Ivan Milutinovic <ivan.milutinovic3@gmail.com>
2019-10-04 16:42:46 +0200

    project and model added

---
+++ Fiscal/Server/Models/InvoiceIteams.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class InvoiceIteam
+    {
+        public int InvoiceIteamsId { get; set; }
+        public int InvoiceId { get; set; }
+        public int ProductID { get; set; }
+        public decimal Quantity { get; set; }
+        public virtual Invoice Invoice { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
